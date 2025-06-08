8207b71bd97687b3279b38539340d9d3c11ab107
8uandj <144869577+8uandj@users.noreply.github.com>
2025-06-09 05:34:24 +0700

    9st

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'https://qair-be.onrender.com/api';
 
 // Cấu hình axios mặc định
 axios.defaults.withCredentials = true;
